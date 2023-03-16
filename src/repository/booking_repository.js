acb42336b551514abf56f857c2c82825e6233bc1
Anvesh Karra <anveshkarra1234@gmail.com>
2023-03-16 16:28:38 +0530

    completed create booking

--- src/repository/booking_repository.js
+++ src/repository/booking_repository.js
@@ -22,8 +22,22 @@ class BookingRepository {
         }
     }
 
-    async update(data) {
-        
+    async update(bookingId , data) {
+        try {
+            const booking = await Booking.findByPk(bookingId);
+            if(data.status) {
+                booking.status = data.status;
+            }
+            await booking.save();
+            return booking;
+        } catch (error) {
+            throw new AppError(
+                'RepositoryError',
+                'Cannot create Booking',
+                'There was some issue creating the booking , please try again later',
+                Statuscodes.INTERNAL_SERVER_ERROR
+            );
+        }
     }
 }
 
---
+++ src/utils/errors/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    AppError : require('./app_error'),
+    ServiceError : require('./service_error'),
+    ValidationError: require('./validation_error')
+}
