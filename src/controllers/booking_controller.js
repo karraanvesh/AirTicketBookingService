61543510d04ec78504d1f552f749058d408c72e0
Anvesh Karra <anveshkarra1234@gmail.com>
2023-03-23 17:14:12 +0530

    Added messageQueues

--- src/controllers/booking_controller.js
+++ src/controllers/booking_controller.js
@@ -14,8 +14,16 @@ class BookingController {
 
     async sendMessageToQueue(req , res) {
         const channel = await createChannel();
-        const data = {message: 'Success'};
-        publishMessage(channel , REMINDER_BINDING_KEY , JSON.stringify(data))
+        const payload = {
+            data: {
+                subject: 'This is a noti from queue',
+                content: 'Some queue will subscribe this',
+                recepientEmail: 'curiowesley123@gmail.com',
+                notificationTime: '2023-03-22T08:30:00'
+            },
+            service: 'CREATE_TICKET'
+        };
+        publishMessage(channel , REMINDER_BINDING_KEY , JSON.stringify(payload));
         return res.status(200).json({
             message: 'Successfully published the event'
         });
